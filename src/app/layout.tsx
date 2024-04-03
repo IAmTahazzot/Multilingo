22ac12c84e245b3bd6311a3e90fad89afb43e0c4
Tahazzot <iamtahazzot@gmail.com>
2024-04-03 14:15:51 +0600

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Multilingo: Language Learning Platform",
+  description: "Learn a new language with MultiLingo! 🌍📚🎉",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-8xl text-slate-200 font-serif'>
+        Hello from the other side.
+      </h1>
+    </div>
+  )
+}
