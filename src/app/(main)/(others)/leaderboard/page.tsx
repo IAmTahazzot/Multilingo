57804636dedfc21ced29a39211a4728d67fc5369
Tahazzot <iamtahazzot@gmail.com>
2024-05-02 07:54:49 +0600

    fix: leader redesign

--- src/app/(main)/(others)/leaderboard/page.tsx
+++ src/app/(main)/(others)/leaderboard/page.tsx
@@ -3,11 +3,7 @@
 import { getLeaderboard } from '@/actions/leaderboard'
 import { Loading } from '@/components/Loading/Loading'
 import { UserPositionCard } from '@/components/Pages/Leaderboard/UserPositionCard'
-import { ICONS } from '@/lib/Icons'
-import Image from 'next/image'
-import Link from 'next/link'
 import { useEffect, useState } from 'react'
-import { useWindowSize } from 'react-use'
 
 export default function LeaderboardPage() {
   const [mount, setMount] = useState(false)
@@ -19,7 +15,6 @@ export default function LeaderboardPage() {
       xp: number
     }[]
   >([])
-  const { width } = useWindowSize()
 
   useEffect(() => {
     const fetchLeaderboardUsers = async () => {
@@ -31,7 +26,6 @@ export default function LeaderboardPage() {
       }
     }
 
-    // just to show loading state :)
     fetchLeaderboardUsers()
   }, [])
 
@@ -48,24 +42,9 @@ export default function LeaderboardPage() {
     )
   }
 
-  const loadImage = (imageUrl: string | null, name: string) => {
-    if (imageUrl) {
-      return <Image src={imageUrl} alt='user' width={40} height={40} className='rounded-full' priority={false} />
-    }
-
-    return (
-      <div className='flex items-center justify-center h-10 w-10 rounded-full bg-black text-white font-display'>
-        {name
-          .split(' ')
-          .map(n => n[0].toUpperCase())
-          .join('')}
-      </div>
-    )
-  }
-
   return (
     <div>
-      <div className="h-10"></div>
+      <div className='h-10'></div>
       <div className='sticky top-0 text-center py-10 border-b border-neutral-200'>
         <h1 className='text-3xl font-display text-neutral-700'>Leaderboard</h1>
         <p className='text-neutral-400 mt-2'>Earn more xp to get higher rank</p>
--- src/components/Pages/Learn/Activity/LeaderboardCard.tsx
+++ src/components/Pages/Learn/Activity/LeaderboardCard.tsx
@@ -1,8 +1,31 @@
+'use client'
+
+import { getUserPosition } from '@/actions/leaderboard'
 import { Card } from '@/components/Card/Card'
 import { NavIcons } from '@/lib/nav'
+import { useUser } from '@clerk/nextjs'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 export const LeaderBoardCard = () => {
+  const { user } = useUser()
+  const [position, setPosition] = useState<number | null>(null)
+
+  useEffect(() => {
+    const fetchPosition = async () => {
+      if (!user) {
+        return null
+      }
+      const position = await getUserPosition(user.id)
+
+      if (position) {
+        setPosition(position)
+      }
+    }
+
+    fetchPosition()
+  }, [user])
+
   return (
     <Card theme='default' className='p-8'>
       <div className='flex items-center justify-between text-[17px] font-display'>
@@ -13,10 +36,20 @@ export const LeaderBoardCard = () => {
       </div>
 
       <div className='grid grid-cols-[60px_1fr] mt-5'>
-        <div className=''>{NavIcons.get('leaderboard')}</div>
+        <div>{NavIcons.get('leaderboard')}</div>
         <div>
-          <h3 className='font-display text-[18px] text-neutral-700'>You&apos;r ranked #30</h3>
-          <p className='text-neutral-500'>You can do better than this</p>
+          {position && (
+            <>
+              <h3 className='font-display text-[18px] text-neutral-700'>You&apos;re ranked #{position}</h3>
+              <p className='text-neutral-500'>
+                {position < 11
+                  ? 'You are in the top 10% of the league'
+                  : position < 51
+                    ? 'You are in the top 50% of the league'
+                    : 'you can do better!'}
+              </p>
+            </>
+          )}
         </div>
       </div>
     </Card>
